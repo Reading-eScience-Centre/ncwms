ac0c474d47a707b1febc8edf2d646a7f78737e09
griffiths.guy <devnull@localhost>
2013-01-31 16:58:23 +0000

    Added framework for XMLViewer on client-side

---
+++ src/main/java/uk/ac/rdg/resc/ncwms/xmlview/client/XMLView.java
@@ -0,0 +1,11 @@
+package uk.ac.rdg.resc.ncwms.xmlview.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class XMLView implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        
+    }
+}
