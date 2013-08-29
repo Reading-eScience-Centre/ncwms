58f82d7697c0dbd47e4fb7297d69e3a7c785eea9
griffiths guy <griffiths.guy@code.google.com>
2013-08-29 10:39:52 +0100

    Bugfixes to make VectorPlugins work end-to-end with GetMap

--- src/main/java/uk/ac/rdg/resc/edal/ncwms/NcwmsCatalogue.java
+++ src/main/java/uk/ac/rdg/resc/edal/ncwms/NcwmsCatalogue.java
@@ -10,19 +10,25 @@ import uk.ac.rdg.resc.edal.dataset.cdm.CdmGridDatasetFactory;
 import uk.ac.rdg.resc.edal.wms.WmsCatalogue;
 
 public class NcwmsCatalogue extends WmsCatalogue {
-    
+
     private Map<String, Dataset> datasets;
-    
+
     public NcwmsCatalogue() throws IOException {
         /*
-         * When finished, this catalogue will read dataset locations/configurations from disk.
+         * When finished, this catalogue will read dataset
+         * locations/configurations from disk.
          * 
-         * For now, we initialise some stuff statically so that we can do an end-to-end test
+         * For now, we initialise some stuff statically so that we can do an
+         * end-to-end test
          */
         datasets = new HashMap<String, Dataset>();
         CdmGridDatasetFactory cdmGridDatasetFactory = new CdmGridDatasetFactory();
-        GridDataset dataset = cdmGridDatasetFactory.createDataset("/home/guy/Data/FOAM_ONE/FOAM_one.ncml");
+        GridDataset dataset = cdmGridDatasetFactory
+                .createDataset("/home/guy/Data/FOAM_ONE/FOAM_one.ncml");
         datasets.put("foam", dataset);
+        for (String varId : dataset.getVariableIds()) {
+            System.out.println(varId + " is part of the foam dataset");
+        }
     }
 
     @Override
@@ -43,7 +49,7 @@ public class NcwmsCatalogue extends WmsCatalogue {
     @Override
     public Dataset getDatasetFromId(String layerName) {
         String[] layerParts = layerName.split("/");
-        if(layerParts.length != 2) {
+        if (layerParts.length != 2) {
             /*
              * TODO
              */
@@ -55,7 +61,7 @@ public class NcwmsCatalogue extends WmsCatalogue {
     @Override
     public String getVariableFromId(String layerName) {
         String[] layerParts = layerName.split("/");
-        if(layerParts.length != 2) {
+        if (layerParts.length != 2) {
             /*
              * TODO
              */
