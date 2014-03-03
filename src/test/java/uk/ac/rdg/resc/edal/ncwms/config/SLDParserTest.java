bf260d7b835222ee5aa7f842800a44a1e7c211d6
charlesjohnroberts <charlesjohnroberts@gmail.com>
2014-03-03 16:57:15 +0000

    Java application to plot test SLD files.

---
+++ src/test/java/uk/ac/rdg/resc/edal/ncwms/config/SLDParserTest.java
@@ -0,0 +1,64 @@
+package uk.ac.rdg.resc.edal.ncwms.config;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
+
+import uk.ac.rdg.resc.edal.dataset.DatasetFactory;
+import uk.ac.rdg.resc.edal.dataset.cdm.CdmGridDatasetFactory;
+import uk.ac.rdg.resc.edal.geometry.BoundingBox;
+import uk.ac.rdg.resc.edal.geometry.BoundingBoxImpl;
+import uk.ac.rdg.resc.edal.graphics.style.MapImage;
+import uk.ac.rdg.resc.edal.graphics.style.sld.StyleSLDParser;
+import uk.ac.rdg.resc.edal.graphics.style.util.FeatureCatalogue;
+import uk.ac.rdg.resc.edal.ncwms.NcwmsCatalogue;
+import uk.ac.rdg.resc.edal.util.PlottingDomainParams;
+
+public class SLDParserTest {
+
+	public static void main(String[] args) {
+		try {
+			// create the plotting domain
+			final int WIDTH = 1024;
+			final int HEIGHT = 512;
+	        BoundingBox bbox = new BoundingBoxImpl(-180, -90, 180, 90, DefaultGeographicCRS.WGS84);
+			PlottingDomainParams params = new PlottingDomainParams(WIDTH, HEIGHT, bbox, null, null, null, null, null);
+
+			// load the datasets from a config file
+			DatasetFactory.setDefaultDatasetFactoryClass(CdmGridDatasetFactory.class);
+			NcwmsConfig config = NcwmsConfig.readFromFile(new File("C:\\Users\\Charles\\.ncWMS-edal\\config.xml"));
+
+			// create the feature catalogue
+			FeatureCatalogue catalogue = new NcwmsCatalogue(config);
+
+			try {
+			    Thread.sleep(3000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}
+			
+			// process each XML file
+			final File folder = new File("N:\\Documents\\SLDInput");
+			for (final File fileEntry : folder.listFiles()) {
+				String fileName = fileEntry.getName();
+				if (fileName.endsWith("xml")) {
+					System.out.println("Processing " + fileName);
+					// create the image from the XML file
+					MapImage mapImage = StyleSLDParser.createImage(fileEntry);
+					BufferedImage image = mapImage.drawImage(params, catalogue);
+					// write the image to a PNG file
+					File outputfile = new File("N:\\Documents\\SLDOutput\\" + fileName.replace("xml", "png"));
+					ImageIO.write(image, "png", outputfile);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		NcwmsConfig.shutdown();
+	}
+
+}
